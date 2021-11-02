e876e0da582907de0b2c875417bee353a79495b1
Eric Hafvenstein <edhavey@null.net>
2021-11-02 04:36:37 -0500

    styled and fixed a lot. almost ready

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 import { OPEN_WEATHER_API_KEY, GOOGLE_API_KEY } from "./api-keys.js";
 
+const parseDayOfTheWeek = num => {
+  switch (num) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thu";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+    default:
+      throw new Error("num is invalid - num must be between 0-6");
+  }
+};
+
 const initApp = () => {
-  const PLACES_API_ENDPOINT =
-    "/api/places/json?";
+  const PLACES_API_ENDPOINT = "/api/places/json?";
   const OPEN_WEATHER_API_ENDPOINT =
     "https://api.openweathermap.org/data/2.5/onecall?";
   const GEOCODE_API_ENDPOINT =
@@ -11,6 +31,8 @@ const initApp = () => {
   const searchForm = document.querySelector("header form");
   const searchBox = document.querySelector("#searchBox");
   const userLocationButton = document.querySelector("#userLocationButton");
+  const weatherCards = document.querySelector("main");
+  const body = document.querySelector("body")
 
   const displayWeather = (weatherData, location) => {
     console.dir(weatherData);
@@ -20,20 +42,19 @@ const initApp = () => {
       </br>
       <span class="state">${location[1] || ""}</span>
       `;
-    const weatherCards = document.querySelector("main");
-    
+
     const today = weatherData.daily[0];
     const current = weatherData.current;
-    
+
     const sunrise = new Date(today.sunrise * 1000);
     const sunset = new Date(today.sunset * 1000);
 
+    console.log({ sunrise }, { sunset });
+
     const dateStrObject = (() => {
       // let weekday, month, dayNum, year;
-      let [weekday, ...date] = new Date()
-        .toDateString()
-        .split(" ");
-      date = date.join(',');
+      let [weekday, ...date] = new Date().toDateString().split(" ");
+      date = date.join(",");
       console.log(weekday, date);
       return {
         weekday,
@@ -41,7 +62,7 @@ const initApp = () => {
       };
     })();
 
-    const overview = `      
+    const forecastOverview = `      
       <div class="location-and-date">
         <div class="location">${locationName}</div>
         <div class="date">
@@ -57,54 +78,86 @@ const initApp = () => {
             }@4x.png">
         </div>
         <div class="temp">
-          ${Math.round(current.temp)}<span class="units">&degF</span>
+          ${Math.round(current.temp)}<span class="unit">&deg;F</span>
         </div>
       </div>
 `;
 
-    const description = `
-      <div class="wrapper">
-        <div class="high-temp">High: ${today.temp.max}&deg;F</div>
-        <div class="low-temp">Low: ${today.temp.min}&deg;F</div>
+    const forecastDescription = `
+      <!-- <div class="wrapper"> -->
+        <div class="high-temp">High: ${today.temp.max}&deg;<span class="unit">F</span></div>
+        <div class="low-temp">Low: ${today.temp.min}&deg;<span class="unit">F</span></div>
         <div class="rain">Rain: ${today.pop}%</div>
-        <div class="wind">Wind: ${current.wind_speed}m/h</div>
+        <div class="wind">Wind: ${current.wind_speed} m/h</div>
         <div class="humidity">Humidity: ${current.humidity}%</div>
-        <div class="visibility">Visibility: ${current.visibility}m</div>
-        <div class="sunrise">Sunrise ${sunrise.getHours()}:${sunrise.getMinutes()} AM</div>
-        <div class="sunset">Sunset ${
-          sunset.getHours() > 12 ? sunset.getHours() - 12 : sunset.getHours()
-        }:${sunset.getMinutes()} PM
+        <div class="visibility">Visibility: ${current.visibility / 100}%</div>
+        <div class="sunrise">Sunrise: ${sunrise.getHours()}:${
+          (sunrise.getMinutes() < 10 ? "0" : "") + sunrise.getMinutes()
+        } AM
         </div>
-      </div>
+        <div class="sunset">Sunset: ${
+          sunset.getHours() > 12 ? sunset.getHours() - 12 : sunset.getHours()
+        }:${(sunset.getMinutes() < 10 ? "0" : "") + sunset.getMinutes()} PM
+        </div>
+      <!-- </div> -->
     `;
 
     const fiveDayForecast = (() => {
       const fiveDays = weatherData.daily.slice(1, 6);
       const divs = [];
       fiveDays.forEach(day => {
-        const div = `<div class="day">${day.pop}, ${day.temp.min}, ${day.temp.max}</div>`;
+        console.log(day);
+        const {
+          pop: rain,
+          temp: { min },
+          temp: { max },
+          dt: date
+        } = day;
+        let dayOfTheWeek = new Date(date * 1000).toString().split(" ")[0];
+        // dayOfTheWeek = parseDayOfTheWeek(dayOfTheWeek);
+        console.log(dayOfTheWeek);
+        const iconURL = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+        const div = `
+          <div class="day">
+            <span class="day-of-the-week">${dayOfTheWeek}</span>
+            <img src="${iconURL}">
+            <span>
+              ${min}&deg;<span class="unit">F</span> - ${max}&deg;<span class="unit">F</span>
+            </span>
+          </div>`;
         divs.push(div);
-      })
-      return divs.join('');
+      });
+      return divs.join("");
     })();
 
     const hourlyForecast = (() => {
-      const twelveHours = weatherData.hourly.slice(0, 12)
+      const twelveHours = weatherData.hourly.slice(1, 13);
       const divs = [];
-      twelveHours.forEach((hour) => {
-        const div = `<div>${hour.temp}</div>`
-        divs.push(div)
-      })
-      return divs.join('')
-    })()
+      const parseHours = (hours) => {
+        let hoursString = ""
+        if (hours > 12) hoursString += (hours - 12 + " PM")
+        else hoursString += hours + (hours === 12 ? " PM" : " AM")
+        return hoursString;
+      }
+      twelveHours.forEach(({temp, dt}) => {
+        const hours = new Date(dt * 1000).getHours()
+        const div = `<span>${parseHours(hours)} | ${temp}&deg;<span class="unit">F</span></span>`;
+        divs.push(div);
+      });
+      return divs.join("");
+    })();
 
     const html = `
-    <div class="card overview">${overview}</div>  
-    <div class="card description">${description}</div>
-    <div class="card five-day">${fiveDayForecast}</div>
-    <div class="card hourly">${hourlyForecast}</div>
-    <div class="card other">Other</div>
+      <div class="card overview">${forecastOverview}</div>  
+      <div class="card description">${forecastDescription}</div>
+      <div class="card five-day">${fiveDayForecast}</div>
+      <div class="card hourly">${hourlyForecast}</div>
+      <div class="card other">Other</div>
     `;
+
+    if (Date.now() < sunset && Date.now() > sunrise) body.style.backgroundImage = "url(/Images/mosi-knife--PVgDgKXgZA-unsplash-edit1.jpg)";
+    else body.style.backgroundImage = "url(/Images/night-sky-cloudy-moon.jpg)"
+
     weatherCards.innerHTML = html;
   };
 
@@ -112,15 +165,15 @@ const initApp = () => {
     const units = "imperial";
     const url = `${OPEN_WEATHER_API_ENDPOINT}lat=${lat}&lon=${lon}&units=${units}&lang=en&appid=${OPEN_WEATHER_API_KEY}`;
     fetch(url)
-      .then((res) => {
+      .then(res => {
         if (!res.ok) throw new Error(res.statusText);
         return res.json();
       })
-      .then((weatherData) => displayWeather(weatherData, locationName))
+      .then(weatherData => displayWeather(weatherData, locationName))
       .catch(console.error);
   };
 
-  const fetchInputLocationData = (e) => {
+  const fetchInputLocationData = e => {
     const input = searchBox.value;
     const url = `${PLACES_API_ENDPOINT}input=${input}&fields=formatted_address,name,geometry,place_id&inputtype=textquery&key=${GOOGLE_API_KEY}`;
     e.preventDefault();
@@ -129,11 +182,11 @@ const initApp = () => {
     // debugger;
     if (input === "") return fetchUserLocationData();
     fetch(url)
-      .then((res) => {
+      .then(res => {
         if (!res.ok) throw new Error(res.statusText);
         return res.json();
       })
-      .then((data) => {
+      .then(data => {
         // console.log(data);
         const lat = data.candidates[0].geometry.location.lat.toPrecision(4);
         const lon = data.candidates[0].geometry.location.lng.toPrecision(4);
@@ -142,19 +195,19 @@ const initApp = () => {
         // console.log(locationName);
         return fetchWeather(lat, lon, locationName);
       })
-      .catch((error) => console.error(error));
+      .catch(error => console.error(error));
   };
 
   const fetchUserLocationName = (lat, lon) => {
     const url = `${GEOCODE_API_ENDPOINT}latlng=${lat},${lon}&key=${GOOGLE_API_KEY}`;
     fetch(url)
-      .then((res) => {
+      .then(res => {
         if (!res.ok) throw new Error(res.statusText);
         return res.json();
       })
-      .then((data) => {
+      .then(data => {
         // console.log(data);
-        const addressComponents = data.results.find((result) => {
+        const addressComponents = data.results.find(result => {
           if (
             result.types[0] === "locality" &&
             result.types[1] === "political" &&
@@ -166,7 +219,7 @@ const initApp = () => {
         // console.log(addressComponents);
         const locationName = [
           addressComponents[0].long_name,
-          addressComponents[2].long_name,
+          addressComponents[2].long_name
         ];
         return fetchWeather(lat, lon, locationName);
       })
@@ -174,11 +227,11 @@ const initApp = () => {
   };
   const fetchUserLocationData = () => {
     // debugger
-    const success = (position) => {
+    const success = position => {
       let { latitude: lat, longitude: lon } = position.coords;
       return fetchUserLocationName(lat, lon);
     };
-    const error = (error) => console.error(error);
+    const error = error => console.error(error);
     navigator.geolocation.getCurrentPosition(success, error);
   };
 
@@ -197,5 +250,5 @@ const initApp = () => {
   // searchBox.focus();
 };
 
-// document.addEventListener('DOMContentLoaded', initApp);
-initApp();
+document.addEventListener("DOMContentLoaded", initApp);
+// initApp();
