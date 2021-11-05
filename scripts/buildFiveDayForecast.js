ed54551fe4f09a0096b6222527c9ff7a707a9be0
Eric Hafvenstein <edhavey@null.net>
2021-11-04 20:36:41 -0500

    refactroring HTML template literals to document fragments & html template tags

--- scripts/buildFiveDayForecast.js
+++ scripts/buildFiveDayForecast.js
@@ -1,6 +1,7 @@
 export const buildFiveDayForecast = weatherData => {
   const fiveDays = weatherData.daily.slice(1, 6);
-  const divs = [];
+  const docFrag = document.createDocumentFragment();
+  // const divs = [];
   fiveDays.forEach(day => {
     const {
       pop: rain,
@@ -10,17 +11,21 @@ export const buildFiveDayForecast = weatherData => {
     } = day;
     const dayOfTheWeek = new Date(date * 1000).toString().split(' ')[0];
     const iconURL = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
-    const div = `
-      <div class="day">
-        <span class="day-of-the-week">${dayOfTheWeek}</span>
-        <img src="${iconURL}">
-        <span>
-          ${Math.round(min)}&deg;<span class="unit">F</span> - ${Math.round(
-      max
-    )}&deg;<span class="unit">F</span>
-        </span>
-      </div>`;
-    divs.push(div);
+
+    const fiveDayTemplate = document.querySelector('#fiveDayTemplate');
+    console.log(fiveDayTemplate);
+
+    // const div = `
+    //   <div class="day">
+    //     <span class="day-of-the-week">${dayOfTheWeek}</span>
+    //     <img src="${iconURL}">
+    //     <span>
+    //       ${Math.round(min)}&deg;<span class="unit">F</span> - ${Math.round(
+    //   max
+    // )}&deg;<span class="unit">F</span>
+    //     </span>
+    //   </div>`;
+    // divs.push(div);
   });
-  return divs.join('');
+  // return divs.join('');
 };
--- scripts/buildHourlyForecast.js
+++ scripts/buildHourlyForecast.js
@@ -2,7 +2,7 @@ export const buildHourlyForecast = weatherData => {
   console.dir(weatherData.hourly);
   const hourlyData = weatherData.hourly.slice();
   const docFrag = document.createDocumentFragment();
-  const divs = [];
+  // const divs = [];
   const parseHourStr = hours => {
     let hourStr = '';
     if (hours > 12) hourStr += hours - 12 + ' PM';
@@ -13,13 +13,12 @@ export const buildHourlyForecast = weatherData => {
     const hours = parseHourStr(new Date(dt * 1000).getHours());
     const div = document.createElement('DIV');
     const span = document.createElement('SPAN');
-    const text = document.createTextNode(`${hours} --- ${Math.round(temp)}`);
+    // const text = document.createTextNode(`${hours} --- ${Math.round(temp)}`);
 
-    // console.dir(text);
     span.className = 'unitSys';
     span.textContent = 'F';
-    div.append(text, span);
-    divs.push(div.outerHTML);
+    div.append(`${hours} --- ${Math.round(temp)}`, span);
+    docFrag.appendChild(div);
   });
-  return divs.join('');
+  return docFrag;
 };
