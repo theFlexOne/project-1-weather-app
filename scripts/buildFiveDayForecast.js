68b92e168f17e0664a5e49119fa06a268195b00a
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2021-11-10 02:34:02 -0600

    display day names with Moment JS

--- scripts/buildFiveDayForecast.js
+++ scripts/buildFiveDayForecast.js
@@ -5,26 +5,31 @@ export const buildFiveDayForecast = ({ weather }) => {
   const fiveDays = weather.daily.slice(1, 6);
   const docFrag = document.createDocumentFragment();
   fiveDays.forEach(day => {
+    const dayName = moment(day.dt * 1000).format('ddd');
+
     const {
       pop: rain,
       temp: { min },
       temp: { max },
-      dt: date,
     } = day;
 
+    //TODO - refactor into docFrag from HTML template
     const div = document.createElement('DIV');
     const span1 = createSpan();
     const img = document.createElement('IMG');
     const span2 = createSpan();
     const span3 = createSpan();
+    const span4 = createSpan();
 
     div.className = 'day wrapper';
     span1.className = 'day-name';
-    span1.textContent = parseDayName(date);
+    span1.textContent = dayName;
     img.src = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
-    span2.textContent = Math.round(min);
+    span2.className = 'min-max';
+    span3.textContent = Math.round(min) + '°F';
     span3.className = 'unitSys';
-    span3.innerText = '°F';
+    span4.textContent = Math.round(max) + '°F';
+    span4.className = 'unitSys';
 
     span2.appendChild(span3);
     div.append(span1, img, span2);
