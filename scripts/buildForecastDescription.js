bb736fcf7c0894c8c518618a7fbc87e7bbec30a0
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2021-11-11 18:44:25 -0600

    fixed sunrise and sunset of forecastDescription with MomentJS and added unit types to forecastDescription

--- scripts/buildForecastDescription.js
+++ scripts/buildForecastDescription.js
@@ -1,33 +1,31 @@
-export const buildForecastDescription = ({ weather }) => {
+export const buildForecastDescription = ({ weather, date }) => {
   const current = weather.current;
   const daily = weather.daily;
   const today = daily[0];
-  const sunrise = moment(today.sunrise * 1000);
-  const sunset = moment(today.sunset * 1000);
+  const sunrise = moment(today.sunrise * 1000).format('h:mm a');
+  const sunset = moment(today.sunset * 1000).format('h:mm a');
+
+  console.log('sunrise', sunrise);
+  console.log('sunset', sunset);
 
   const docFrag = document
     .querySelector('#descriptionTemplate')
     .cloneNode(true).content;
 
-  docFrag.querySelector('.high-temp').textContent = Math.round(today.temp.max);
-  docFrag.querySelector('.low-temp').textContent = Math.round(today.temp.min);
-  docFrag.querySelector('.rainChance').textContent = today.pop;
-  docFrag.querySelector('.wind').textContent = current.wind_speed + 'm/h';
-  docFrag.querySelector('.humidity').textContent = current.humidity;
+  docFrag.querySelector('.high-temp').textContent =
+    'High - ' + Math.round(today.temp.max) + '°F';
+  docFrag.querySelector('.low-temp').textContent =
+    'Low - ' + Math.round(today.temp.min) + '°F';
+  docFrag.querySelector('.rainChance').textContent =
+    'Rain - ' + today.pop + '%';
+  docFrag.querySelector('.wind').textContent =
+    'Wind - ' + current.wind_speed + 'm/h';
+  docFrag.querySelector('.humidity').textContent =
+    'Humidity - ' + current.humidity;
   docFrag.querySelector('.visibility').textContent =
-    current.visibility / 100 + '%';
-  docFrag.querySelector('.sunrise').textContent =
-    sunrise.hour() +
-    ':' +
-    (sunrise.minutes() < 10 ? '0' : '') +
-    sunrise.minutes() +
-    ' AM';
-  docFrag.querySelector('.sunset').textContent =
-    sunset.hours() > 12
-      ? sunset.hours() - 12
-      : sunset.hours() + ':' + sunset.minutes() < 10
-      ? '0'
-      : '' + sunset.minutes() + ' PM';
+    'Visibility - ' + current.visibility / 100 + '%';
+  docFrag.querySelector('.sunrise').textContent = 'Sunrise - ' + sunrise;
+  docFrag.querySelector('.sunset').textContent = 'Sunset - ' + sunset;
 
   return docFrag;
 };
